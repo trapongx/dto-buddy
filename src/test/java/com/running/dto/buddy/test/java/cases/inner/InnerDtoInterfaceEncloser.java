d6c464e02604f899886a50f1a9f9d084071cc1b5
Teerapong Suksuwan <trapongx@gmail.com>
2025-07-03 11:24:49 +0700

    Add Java test for the case of inner base classes

---
+++ src/test/java/com/running/dto/buddy/test/java/cases/inner/InnerDtoInterfaceEncloser.java
@@ -0,0 +1,9 @@
+package com.running.dto.buddy.test.java.cases.inner;
+
+public interface InnerDtoInterfaceEncloser {
+    interface InnerDtoInterface {
+        String getName();
+        Integer getAge();
+        String getEmail();
+    }
+}
---
+++ src/test/java/com/running/dto/buddy/test/java/cases/inner/InnerDtoInterfaceTest.java
@@ -0,0 +1,21 @@
+package com.running.dto.buddy.test.java.cases.inner;
+
+import com.runninglane.dto.buddy.DtoBuddy;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class InnerDtoInterfaceTest {
+
+    @Test
+    public void testImplement()
+    {
+        Class<?> concreteClass = DtoBuddy.implementor()
+            .withBaseClass(InnerDtoInterfaceEncloser.InnerDtoInterface.class)
+            .implement();
+        assertNotNull(concreteClass);
+        assertTrue(InnerDtoInterfaceEncloser.InnerDtoInterface.class. isAssignableFrom (concreteClass));
+    }
+
+}
